ad12072d86d01bcc94853b0026eba9231c80a77e
marb17 <marvellchristiansutanto@gmail.com>
2026-05-03 22:29:34 +0800

    idk what im doing

--- src/logic.py
+++ src/logic.py
@@ -7,3 +7,22 @@ class ClassName:
         ...
         return ...
 """
+
+import hardware
+
+class MainPower:
+    """
+    Main power switch, if not enabled, other systems cannot be activated
+    """
+    @staticmethod
+    def is_powered(panel_context: dict) -> bool:
+        """
+        Requires a switch object to be passed in as panel_context["main_power"]
+        """
+        if type(panel_context["main_power"]) is not hardware.Switch:
+            raise TypeError("Main power switch must be a Switch object!")
+
+        if panel_context["main_power"].is_pressed:
+            return True
+        else:
+            return False
